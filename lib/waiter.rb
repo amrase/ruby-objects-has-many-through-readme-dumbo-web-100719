aace62a586a29459fcc8b07be83f1db69eddc1db
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-11 21:43:48 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,28 @@
+
 class Waiter
+
+  attr_accessor :name,:years 
+  @@all =[ ]
+  def initialize(name,year)
+    @name=name 
+    @years=year
+    
+    
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end
+  
+  def new_meal(customer,total,tip)
+    Meal.new(self, customer,total,tip)
+
+  end
+  
+  # def meals 
+  #   Customer.all.select do | right_waiter | 
+  #     right_waiter.meal== self 
+  #   end  
 
 end
